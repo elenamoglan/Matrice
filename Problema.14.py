3c4cd285ba062266ee1dc3b0953b1ed28bceb799
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-10-04 21:16:26 +0300

    Update Problema.14.py

--- Problema.14.py
+++ Problema.14.py
@@ -4,33 +4,26 @@ print('Matricea introdusa:')
 for i in range(len(A)):
     print(A[i])
 s1 = 0
-for i in range(0,len(A)):
-    s1 += A[i][i]
 s2 = 0
 for i in range(0,len(A)):
+    s1 += A[i][i]
     s2 += A[len(A)-i-1][i]
 print(f'Suma componentelor diagonalei secundare este {s2}, iar suma componentelor diagonalei principale este {s1}')
 s3=0
+s4=0
+s5=0
+s6=0
 for i in range(0,len(A)):
     for j in range(0,len(A)):
         if i<j:
             s3+=A[i][j]
-print(f'Suma componentelor mai sus de diagonala principală este {s3}')
-s4=0
-for i in range(0,len(A)):
-    for j in range(0,len(A)):
         if i>j:
             s4+=A[i][j]
-print(f'Suma componentelor mai jos de diagonala principală este {s4}')
-s5=0
-for i in range(0,len(A)):
-    for j in range(0,len(A)):
         if i+j<n-1:
             s5+=A[i][j]
-print(f'Suma componentelor mai sus de diagonala secundara este {s5}')
-s6=0
-for i in range(0,len(A)):
-    for j in range(0,len(A)):
         if i+j>n-1:
             s6+=A[i][j]
+print(f'Suma componentelor mai sus de diagonala principală este {s3}')     
+print(f'Suma componentelor mai jos de diagonala principală este {s4}')
+print(f'Suma componentelor mai sus de diagonala secundara este {s5}')    
 print(f'Suma componentelor mai jos de diagonala secundara este {s6}')
